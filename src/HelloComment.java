9b457f8985277ee0638c40c20827780698502f6f
Jae-Hong-Kim <newfly101@naver.com>
2024-07-02 16:00:39 +0900

    0-1. java comment!

---
+++ src/HelloComment.java
@@ -0,0 +1,10 @@
+public class HelloComment {
+    public static void main(String[] args) {
+        System.out.println("sout => system.out.println");
+        // java 주석 추가
+
+        /*
+        여러 줄 주석 추가
+        * */
+    }
+}
