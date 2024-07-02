d625c7038952df891d140bb62b89626ea477998c
Jae-Hong-Kim <newfly101@naver.com>
2024-07-02 16:17:11 +0900

    1-2. 변수 값 변경

---
+++ src/variable/Var2.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int number;
+        number = 10;
+        System.out.println(number);
+        number = 20;
+        System.out.println(number);
+    }
+}
