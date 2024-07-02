c63f71704ccf64a42b89586c9190bccbf486dfaa
Jae-Hong-Kim <newfly101@naver.com>
2024-07-02 16:15:03 +0900

    1-1. 변수 시작

---
+++ src/variable/Var1.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println("psvm => public static void main");
+
+        int number;      // 변수 선언
+        number = 10;     // 변수 초기화
+
+        System.out.println("number => " + number);      // 변수 값 읽기
+    }
+}
