1816a4f6929ddf7cf65d74927a01dc3188b3f6fb
Jae-Hong-Kim <newfly101@naver.com>
2024-07-02 16:33:18 +0900

    1-4. 변수 타입1

---
+++ src/variable/Var4.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        int a = 100; // 정수
+        double d = 10.5; // 실수
+        boolean tf = true; // 불리언(true / false)
+        char c = 'A'; // 문자 하나
+        String s = "Hello World"; // 문자열
+
+        System.out.println(a);
+        System.out.println(d);
+        System.out.println(tf);
+        System.out.println(c);
+        System.out.println(s);
+    }
+}
