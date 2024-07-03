32ea8623b98d590126876a03dab8fd63974bbbef
Jae-Hong-Kim <newfly101@naver.com>
2024-07-03 09:44:52 +0900

    3. 조건문 - switch문으로 변경

--- src/condition/ConditionSwitch.java
+++ src/condition/ConditionSwitch.java
@@ -13,14 +13,18 @@ public class ConditionSwitch {
     public static void main(String[] args) {
         int grade = 3;
         int coupon = 0;
-        if (grade == 1) {
-            coupon = 1000;
-        } else if (grade == 2) {
-            coupon = 2000;
-        } else if (grade == 3) {
-            coupon = 3000;
-        } else {
-            coupon = 500;
+        switch (grade) {
+            case 1:
+                coupon = 1000;
+                break;
+            case 2:
+                coupon = 2000;
+                break;
+            case 3:
+                coupon = 3000;
+                break;
+            default:
+                coupon = 500;
         }
         System.out.println("발급받은 쿠폰: "+coupon);
     }
