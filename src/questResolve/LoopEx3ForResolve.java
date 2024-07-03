4a449d25b62b2c50e24bb8206a968e5c86eb2de5
Jae-Hong-Kim <newfly101@naver.com>
2024-07-03 20:54:20 +0900

    4. 반복문 관련 문제3 풀이 추가

---
+++ src/questResolve/LoopEx3ForResolve.java
@@ -0,0 +1,12 @@
+package questResolve;
+
+public class LoopEx3ForResolve {
+    public static void main(String[] args) {
+        int sum = 0;
+        int max = 13;
+        for (int i = 1; i <= max; i++) {
+            sum += i;
+            System.out.println(sum);
+        }
+    }
+}
---
+++ src/questResolve/LoopEx3WhileResolve.java
@@ -0,0 +1,14 @@
+package questResolve;
+
+public class LoopEx3WhileResolve {
+    public static void main(String[] args) {
+        int sum = 0;
+        int max = 13;
+        int i = 1;
+        while (i <= max) {
+            sum = sum + i;
+            System.out.println(sum);
+            i++;
+        }
+    }
+}
