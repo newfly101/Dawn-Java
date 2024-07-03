0458512922269c319b21ce307b05f9872f8b545c
Jae-Hong-Kim <newfly101@naver.com>
2024-07-03 19:50:08 +0900

    4-3. 반복문 DO-WHILE문

---
+++ src/loop/DoWhile.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 10;
+        do {
+            System.out.println("현재 숫자는 " + i);
+            i++;
+        }
+        while (i < 3);
+        // do 에 의해서 한번 실행하고 종료 된다.
+    }
+}
