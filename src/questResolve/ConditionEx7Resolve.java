c87c9113321aa0581807e30566892ddc0a6deed0
Jae-Hong-Kim <newfly101@naver.com>
2024-07-03 17:50:43 +0900

    3. 조건문 관련 문제7 풀이 추가

---
+++ src/questResolve/ConditionEx7Resolve.java
@@ -0,0 +1,9 @@
+package questResolve;
+
+public class ConditionEx7Resolve {
+    public static void main(String[] args) {
+        int x = 3;
+        String result = x%2 == 0 ? "짝수" : "홀수";
+        System.out.println("x = "+ x + ", " + result);
+    }
+}
