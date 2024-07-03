5c1a1e087234997794cb595d40301f330ac1dcc8
Jae-Hong-Kim <newfly101@naver.com>
2024-07-03 20:50:56 +0900

    4. 반복문 관련 문제2 풀이 추가

---
+++ src/questResolve/LoopEx2WhileResolve.java
@@ -0,0 +1,13 @@
+package questResolve;
+
+public class LoopEx2WhileResolve {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num <= 20) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+            num++;
+        }
+    }
+}
