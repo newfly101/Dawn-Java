4d0d8c991b9311afaea11b67291e95b8354540d7
Jae-Hong-Kim <newfly101@naver.com>
2024-07-22 17:10:27 +0900

    6. Scanner 반복 예제3 - 답추가(문자열 입력시 오류 조건 처리 추가)

---
+++ src/scanner/Scanner5.java
@@ -0,0 +1,26 @@
+package scanner;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Scanner5 {
+    public static void main(String[] args) {
+        /* 사용자가 입력한 숫자의 모든 합을 계산하는 문제 */
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+
+        while (true) {
+            try {
+                System.out.print("숫자를 입력(0입력시 종료) : ");
+                int num = sc.nextInt();
+                if (num == 0) break; // 0입력시 종료
+                sum += num;
+            } catch (InputMismatchException e) {
+                System.out.println("숫자를 입력하세요!");
+                sc.next();
+            }
+        }
+        System.out.println("입력한 모든 정수의 합 : " + sum);
+    }
+}
