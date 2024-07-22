c33553497ca6078a2dd90343fe14514d8bfd9b67
Jae-Hong-Kim <newfly101@naver.com>
2024-07-22 16:58:02 +0900

    6. Scanner 반복 예제1

---
+++ src/scanner/Scanner3.java
@@ -0,0 +1,11 @@
+package scanner;
+
+public class Scanner3 {
+    public static void main(String[] args) {
+        /*
+        * 사용자가 입력한 문자열을 그대로 출력하는 예제
+        * exit 입력시 프로그램 종료
+        * 프로그램을 반복해서 실행
+        */
+    }
+}
