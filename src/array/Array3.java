a719e94b6bf5793a927e7fcd6fe788433c7c9906
Jae-Hong-Kim <newfly101@naver.com>
2024-07-23 10:41:42 +0900

    7. 2차원 배열 - 리팩터링 2

--- src/array/Array3.java
+++ src/array/Array3.java
@@ -2,13 +2,10 @@ package array;
 
 public class Array3 {
     public static void main(String[] args) {
-        int[][] arr = new int[2][3];
-        arr[0][0] = 1;
-        arr[0][1] = 2;
-        arr[0][2] = 3;
-        arr[1][0] = 4;
-        arr[1][1] = 5;
-        arr[1][2] = 6;
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+        };
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
