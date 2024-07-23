26018254fd609dddda2e5265c909ea38a57fd3fd
Jae-Hong-Kim <newfly101@naver.com>
2024-07-23 10:09:29 +0900

    6. Scanner 관련 문제9 풀이 추가(수정)

--- src/questResolve/ScannerEx9Resolve.java
+++ src/questResolve/ScannerEx9Resolve.java
@@ -22,17 +22,13 @@ public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int sum = 0;
     int count = 0;
-    double average = 0.0;
+    int num = 0;
     System.out.println("숫자를 입력하세요. 입력을 중단하려면 -1을 입력하세요.");
-    while (true) {
-        int num = sc.nextInt();
-        if (num == -1) {
-            break;
-        }
+    while ((num = sc.nextInt()) != -1) {
         sum += num;
         count++;
-        average = (double) sum / count;
     }
+    double average = (double) sum / count;
     System.out.println("입력한 숫자들의 합계: "+ sum);
     System.out.println("입력한 숫자들의 평균: "+ average);
 }
