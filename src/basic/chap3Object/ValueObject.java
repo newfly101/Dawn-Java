7638fe2d7455a6e4dd4653fa68fe8830d79fa554
Jae-Hong-Kim <newfly101@naver.com>
2024-07-25 07:03:19 +0900

    3. 객체 지향 프로그래밍 - 클래스 내부에 속성(데이터)과 기능(메서드)을 함께 포함할 수 있다. 클래스 내부에 멤버 변수 뿐만 아니라 메서드도 함께 포함

---
+++ src/basic/chap3Object/ValueObject.java
@@ -0,0 +1,9 @@
+package basic.chap3Object;
+
+public class ValueObject {
+    int value;
+    void add() {
+        value++;
+        System.out.println("숫자 증가 value=" + value);
+    }
+}
---
+++ src/basic/chap3Object/ValueObjectMain.java
@@ -0,0 +1,11 @@
+package basic.chap3Object;
+
+public class ValueObjectMain {
+    public static void main(String[] args) {
+        ValueObject vo = new ValueObject();
+        vo.add();
+        vo.add();
+        vo.add();
+        System.out.println("최종 숫자=" + vo.value);
+    }
+}
