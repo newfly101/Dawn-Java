96acefd5adfa202a45c264196a7ca63992b9c794
Jae-Hong-Kim <newfly101@naver.com>
2024-07-25 07:40:17 +0900

    4. 생성자 - this

--- src/basic/chap4Construct/MemberInit.java
+++ src/basic/chap4Construct/MemberInit.java
@@ -4,4 +4,11 @@ public class MemberInit {
     String name;
     int age;
     int grade;
+
+    void initMember(String name, int age, int grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        // this : instance 자신의 참조값을 가리킴
+    }
 }
