33345ecb23104c8b082b2401c0e659ab2b9513c3
Jae-Hong-Kim <newfly101@naver.com>
2024-07-25 07:41:04 +0900

    4. 생성자 - this

--- src/basic/chap4Construct/MethodInitMain3.java
+++ src/basic/chap4Construct/MethodInitMain3.java
@@ -13,9 +13,4 @@ public class MethodInitMain3 {
             System.out.println("이름: " + m.name + ", 나이: "+ m.age + ", 성적: "+ m.grade);
         }
     }
-    static void initMember (MemberInit m, String name, int age, int grade) {
-        m.name = name;
-        m.age = age;
-        m.grade = grade;
-    }
 }
