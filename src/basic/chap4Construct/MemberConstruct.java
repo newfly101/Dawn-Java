4f56c293a3e06d6cf979cc293e90de2723d52d76
Jae-Hong-Kim <newfly101@naver.com>
2024-07-25 07:51:47 +0900

    4. 생성자 - 생성자 호출(오버로딩)

--- src/basic/chap4Construct/MemberConstruct.java
+++ src/basic/chap4Construct/MemberConstruct.java
@@ -5,6 +5,13 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    // 메서드 오버로딩 처럼 매개변수만 다르게 해서 여러 생성자 제공
+    MemberConstruct(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.grade = 50;
+    }
+
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name="+name +", age="+age+", grade="+grade);
         this.name = name;
