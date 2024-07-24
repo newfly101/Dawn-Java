98ae554b337e66c56405cdc86ea0b2f990484d3d
Jae-Hong-Kim <newfly101@naver.com>
2024-07-25 06:52:49 +0900

    3. 객체 지향 프로그래밍 - 음악 플레이어 만들기

--- src/basic/chap3Object/MusicPlayerMain1.java
+++ src/basic/chap3Object/MusicPlayerMain1.java
@@ -7,6 +7,28 @@ public class MusicPlayerMain1 {
     //1. 음악 플레이어를 켜고 끌 수 있어야 한다.
     //2. 음악 프레이어의 볼륨을 증가, 감소할 수 있어야 한다.
     //3. 음악 플레이어의 상태를 확인할 수 있어야 한다.
+    public static void main(String[] args) {
+        int volume = 0;
+        boolean isOn = false;
+        System.out.println("음악 플레이어를 시작합니다.");
+        isOn = true;
+
+        volume++;
+        System.out.println("음악 플레이어 볼륨:"+volume);
+
+        volume--;
+        System.out.println("음악 플레이어 볼륨:"+volume);
+
+        System.out.println("음악 플레이어 상태 확인");
+        if(isOn) {
+            System.out.println("음악 플레이어 ON, 볼륨:"+volume);
+        } else {
+            System.out.println("음악 플레이어 OFF");
+        }
+
+        isOn = false;
+        System.out.println("음악 플레이어를 종료합니다");
+    }
     //예시 출력: ```
     //음악 플레이어를 시작합니다
     //음악 플레이어 볼륨:1
