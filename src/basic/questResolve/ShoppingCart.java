7ae092d146def9861288b409359ebe2dcd8f9d09
Jae-Hong-Kim <newfly101@naver.com>
2024-07-25 14:08:16 +0900

    5. 접근 제어자 관련 문제2 풀이 추가

--- src/basic/questResolve/ShoppingCart.java
+++ src/basic/questResolve/ShoppingCart.java
@@ -5,8 +5,12 @@ public class ShoppingCart {
      private int itemCount = 0;
 
      public void addItem(Item item) {
-          items[itemCount] = item;
-          itemCount++;
+          if  (itemCount < items.length) {
+               items[itemCount] = item;
+               itemCount++;
+          } else {
+               System.out.println("장바구니가 가득 찼습니다.");
+          }
      }
      public void displayItems() {
           System.out.println("장바구니 상품 출력");
