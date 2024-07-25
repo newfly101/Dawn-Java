0bbd24fd479caa56f3244b74883244171c5df734
Jae-Hong-Kim <newfly101@naver.com>
2024-07-25 15:11:21 +0900

    6. 스택 영역 - 스택과 힙 영역

---
+++ src/basic/chap6Memory/Data.java
@@ -0,0 +1,12 @@
+package basic.chap6Memory;
+
+public class Data {
+    private int value;
+
+    public Data(int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
+}
