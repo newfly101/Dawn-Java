4f2811356862da161c9efcc3e32aaa15f36bbf9a
Jae-Hong-Kim <newfly101@naver.com>
2024-07-26 17:04:31 +0900

    6. 자바 메모리 구조와 static 관련 문제1 풀이 추가

---
+++ src/basic/questResolve/Car.java
@@ -0,0 +1,16 @@
+package basic.questResolve;
+
+public class Car {
+    private String carName;
+    private static int count;
+
+
+    public Car(String carName) {
+        this.carName = carName;
+        System.out.println("차량 구입, 이름: "+carName);
+        count++;
+    }
+    public static void showTotalCars() {
+        System.out.println("구매한 차량 수: "+ count);
+    }
+}
