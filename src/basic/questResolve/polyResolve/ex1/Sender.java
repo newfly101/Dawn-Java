aea728cf4dd1bb7daea732094db53c1ef4ee9dfd
Jae-Hong-Kim <newfly101@naver.com>
2024-07-27 20:55:38 +0900

    11. 다형성 관련 문제1 풀이 추가

---
+++ src/basic/questResolve/polyResolve/ex1/Sender.java
@@ -0,0 +1,5 @@
+package basic.questResolve.polyResolve.ex1;
+
+public interface Sender {
+    void sendMessage(String message);
+}
---
+++ src/basic/questResolve/polyResolve/ex1/SmsSender.java
@@ -0,0 +1,8 @@
+package basic.questResolve.polyResolve.ex1;
+
+public class SmsSender implements Sender{
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("SMS를 발송합니다: " + message);
+    }
+}
