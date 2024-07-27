2df8a36c2457eecc22aa96d3d1a0904d8a0469d1
Jae-Hong-Kim <newfly101@naver.com>
2024-07-27 21:15:59 +0900

    11. 다형성 관련 문제3 풀이 추가

--- src/basic/questResolve/polyResolve/ex2/PolyEx3Resolve.java
+++ src/basic/questResolve/polyResolve/ex2/PolyEx3Resolve.java
@@ -1,6 +1,9 @@
 package basic.questResolve.polyResolve.ex2;
 
-public class PolyEx2Resolve {
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class PolyEx3Resolve {
     // 문제2: 결제 시스템 개발
     //여러분은 기대하던 결제 시스템 개발팀에 입사하게 되었다.
     //이 팀은 현재 2가지 결제 수단을 지원한다. 앞으로 5개의 결제 수단을 추가로 지원할 예정이다.
@@ -11,21 +14,42 @@ public class PolyEx2Resolve {
     //단 프로그램을 실행하는 PayMain0 코드는 변경하지 않고, 그대로 유지해야 합니다.
     //리펙토링 후에도 실행 결과는 기존과 같아야 합니다.
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         PayService payService = new PayService();
-        //kakao 결제
-        String payOption1 = "kakao";
-        int amount1 = 5000;
-        payService.processPay(payOption1, amount1);
-        //naver 결제
-        String payOption2 = "naver";
-        int amount2 = 10000;
-        payService.processPay(payOption2, amount2);
-        //잘못된 결제 수단 선택
-        String payOption3 = "bad";
-        int amount3 = 15000;
-        payService.processPay(payOption3, amount3);
+        while (true) {
+            try {
+                // 사용자 입력 추가
+                System.out.print("결제 수단을 입력하세요: ");
+                String option = sc.nextLine();
+                if (option.equals("exit")) {
+                    System.out.println("프로그램을 종료합니다.");
+                    break;
+                }
+                System.out.print("결제 금액을 입력하세요: ");
+                int amount = sc.nextInt();
+                sc.nextLine(); //  입력 버퍼 초기화
+
+                payService.processPay(option, amount);
+
+            } catch (InputMismatchException e) {
+                System.out.println("잘못 입력했습니다. 처음으로 돌아갑니다");
+                continue;
+            }
+        }
+
+
     }
-    //실행 결과 ```
+    //실행 결과 ex3
+    //결제 수단을 입력하세요:kakao
+    //결제 금액을 입력하세요:5000
+    //결제를 시작합니다: option=kakao, amount=5000
+    //카카오페이 시스템과 연결합니다.
+    //5000원 결제를 시도합니다.
+    //결제가 성공했습니다.
+    //결제 수단을 입력하세요:exit
+    //프로그램을 종료합니다.
+
+
     //결제를 시작합니다: option=kakao, amount=5000
     //카카오페이 시스템과 연결합니다.
     //5000원 결제를 시도합니다.
