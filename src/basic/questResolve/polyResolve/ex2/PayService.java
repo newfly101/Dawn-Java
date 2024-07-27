373aa982b575dd5d9d3951bab7000e6f4bb10d88
Jae-Hong-Kim <newfly101@naver.com>
2024-07-27 21:08:29 +0900

    11. 다형성 관련 문제2 풀이 추가 - 정답지 내용 추가

--- src/basic/questResolve/polyResolve/ex2/PayService.java
+++ src/basic/questResolve/polyResolve/ex2/PayService.java
@@ -2,19 +2,12 @@ package basic.questResolve.polyResolve.ex2;
 
 public class PayService {
     public void processPay(String option, int amount) {
-        boolean result;
         System.out.println("결제를 시작합니다: option=" + option + ", amount=" +
                 amount);
-        if (option.equals("kakao")) {
-            Pay pay = new KakaoPay();
-            result = pay.pay(amount);
-        } else if (option.equals("naver")) {
-            Pay pay = new NaverPay();
-            result = pay.pay(amount);
-        } else {
-            System.out.println("결제 수단이 없습니다.");
-            result = false;
-        }
+
+        Pay pay = PayStore.findPay(option);
+        boolean result =  pay.pay(amount);
+
         if (result) {
             System.out.println("결제가 성공했습니다.");
         } else {
