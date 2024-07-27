2674c167334feda51564f95b43f6c793a8482e7e
Jae-Hong-Kim <newfly101@naver.com>
2024-07-27 20:35:38 +0900

    11. 다형성 - 역할과 구현 예제2

--- src/basic/chap11Poly/car0/CarMain.java
+++ src/basic/chap11Poly/car0/CarMain.java
@@ -6,5 +6,10 @@ public class CarMain {
         K3Car k3Car = new K3Car();
         driver.setK3Car(k3Car);
         driver.drive();
+
+        // 추가
+        Model3Car model3Car = new Model3Car();
+        driver.setModel3Car(model3Car);
+        driver.drive();
     }
 }
