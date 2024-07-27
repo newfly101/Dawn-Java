66c5dbb725d42510d0135d3bd455226a17134edb
Jae-Hong-Kim <newfly101@naver.com>
2024-07-27 15:22:45 +0900

    9. 다형성 -> method overriding

---
+++ src/basic/chap9Poly/basic/overriding/OverridingMain.java
@@ -0,0 +1,26 @@
+package basic.chap9Poly.basic.overriding;
+
+public class OverridingMain {
+    public static void main(String[] args) {
+
+        // 자식 변수가 있는 인스턴스 참조
+        Child child = new Child();
+        System.out.println("Child -> Child");
+        System.out.println("value = " + child.value);
+        child.method();
+        System.out.println();
+
+        // 부모 변수가 있는 인스턴스 참조
+        Parent parent = new Parent();
+        System.out.println("Parent -> Parent");
+        System.out.println("value = " + parent.value);
+        parent.method();
+        System.out.println();
+
+        // 부모 변수가 자식 인스턴스 참조(다형적 참조)
+        Parent poly = new Child();
+        System.out.println("Parent -> Child");
+        System.out.println("value = " + poly.value);
+        poly.method(); // 메서드 오버라이딩
+    }
+}
---
+++ src/basic/chap9Poly/basic/overriding/Parent.java
@@ -0,0 +1,9 @@
+package basic.chap9Poly.basic.overriding;
+
+public class Parent {
+    public String value = "parent";
+
+    public void method() {
+        System.out.println("Parent method called");
+    }
+}
