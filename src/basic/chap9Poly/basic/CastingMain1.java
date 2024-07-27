cd2d32438325f5167c86e176a479b5cff973400f
Jae-Hong-Kim <newfly101@naver.com>
2024-07-27 14:52:21 +0900

    9. 다형성 -> 다운 캐스팅

--- src/basic/chap9Poly/basic/CastingMain1.java
+++ src/basic/chap9Poly/basic/CastingMain1.java
@@ -1,6 +1,6 @@
 package basic.chap9Poly.basic;
 
-public class CastingMain {
+public class CastingMain1 {
     public static void main(String[] args) {
         // 다운 캐스팅 : 부모 타입을 잠시 자식 타입으로 변경
         System.out.println("Parent -> child");
