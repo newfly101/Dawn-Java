d5aaf8d73b1a90af6ed578df24d92af386169b16
Jae-Hong-Kim <newfly101@naver.com>
2024-07-27 09:38:10 +0900

    8. 상속 - super 생성자 : this() 를 쓰는 경우 super() 생략 가능 해짐

--- src/basic/chap8Extends/super2Package/ClassB.java
+++ src/basic/chap8Extends/super2Package/ClassB.java
@@ -2,7 +2,8 @@ package basic.chap8Extends.super2Package;
 
 public class ClassB extends ClassA{
     public ClassB(int a) {
-        super(); // 기본 생성자 생략 가능
+        this(a, 0); // 기본 생성자 생략 가능
+//        super(); // 기본 생성자 생략 가능
         System.out.println("ClassB constructor a = " + a);
     }
     public ClassB(int a, int b) {
--- src/basic/chap8Extends/super2Package/SuperMain.java
+++ src/basic/chap8Extends/super2Package/SuperMain.java
@@ -2,6 +2,7 @@ package basic.chap8Extends.super2Package;
 
 public class SuperMain {
     public static void main(String[] args) {
-        ClassC classC = new ClassC();
+//        ClassC classC = new ClassC();
+        ClassB classB = new ClassB(100);
     }
 }
