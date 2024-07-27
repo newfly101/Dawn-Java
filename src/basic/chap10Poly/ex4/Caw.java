69cedc6370217856a07994dd47fcc420a39eae92
Jae-Hong-Kim <newfly101@naver.com>
2024-07-27 19:51:46 +0900

    10. 다형성 활용 -> 추상 클래스 사용

---
+++ src/basic/chap10Poly/ex4/Caw.java
@@ -0,0 +1,8 @@
+package basic.chap10Poly.ex4;
+
+public class Caw extends AbstractAnimal {
+    @Override
+    public void sound() {
+        System.out.println("Caw Sound");
+    }
+}
---
+++ src/basic/chap10Poly/ex4/Dog.java
@@ -0,0 +1,8 @@
+package basic.chap10Poly.ex4;
+
+public class Dog extends AbstractAnimal {
+    @Override
+    public void sound() {
+        System.out.println("Dog sound");
+    }
+}
