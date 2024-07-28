ddd6270f8a347f006f92f5a694c44da26c276e32
Jae-Hong-Kim <newfly101@naver.com>
2024-07-28 10:23:47 +0900

    1. Object 클래스 - java.lang 패키지

---
+++ src/mid1/chap1Object/lang/LangMain.java
@@ -0,0 +1,9 @@
+package mid1.chap1Object.lang;
+
+import java.lang.System; // 생략 가능
+
+public class LangMain {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
