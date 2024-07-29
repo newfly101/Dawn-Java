ea1aa68c76ab469945706ab502b174fff86968a9
Jae-Hong-Kim <newfly101@naver.com>
2024-07-29 13:55:34 +0900

    2. 불변 객체 - 불변 객체에서 값 변경하는 법

---
+++ src/mid1/chap2Immutable/ImmutableObj.java
@@ -0,0 +1,16 @@
+package mid1.chap2Immutable;
+
+public class ImmutableObj {
+    private final int value;
+
+    public ImmutableObj(int value) {
+        this.value = value;
+    }
+    public ImmutableObj add(int addValue) {
+        int result = value + addValue;
+        return new ImmutableObj(result);
+    }
+    public int getValue() {
+        return value;
+    }
+}
