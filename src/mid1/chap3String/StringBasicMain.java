3f57754f636e5e551b39c9bfd6c55e924acf017e
Jae-Hong-Kim <newfly101@naver.com>
2024-07-30 12:17:09 +0900

    3. String 클래스 - 시작

---
+++ src/mid1/chap3String/StringBasicMain.java
@@ -0,0 +1,11 @@
+package mid1.chap3String;
+
+public class StringBasicMain {
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = new String("hello"); // new String은 불필요한
+
+        System.out.println("str1 = " + str1);
+        System.out.println("str2 = " + str2);
+    }
+}
