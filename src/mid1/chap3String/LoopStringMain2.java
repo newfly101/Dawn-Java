3d43c074f03cc91434cd94d3d25c0fcc08d289b9
Jae-Hong-Kim <newfly101@naver.com>
2024-07-30 12:56:27 +0900

    3. String 클래스 - StringBuilder 최적화가 좋은 이유

---
+++ src/mid1/chap3String/LoopStringMain2.java
@@ -0,0 +1,16 @@
+package mid1.chap3String;
+
+public class LoopStringMain2 {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 100000; i++) {
+            sb.append("Hello Java ");
+        }
+        String result = sb.toString();
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("result = " + result);
+        System.out.println("time = " + (endTime - startTime) + "ms");
+    }
+}
