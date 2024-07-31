28ccf236ae3caabc5de340b729b705e57ab18042
Jae-Hong-Kim <newfly101@naver.com>
2024-07-31 19:30:52 +0900

    3. String 클래스 관련 문제4  풀이 추가

---
+++ src/mid1/questResolve/string/StringEx4Resolve.java
@@ -0,0 +1,17 @@
+package mid1.questResolve.string;
+
+public class StringEx4Resolve {
+    // 문제4 - substring()
+    //문제 설명
+    //substring() 을 사용해서, hello 부분과 .txt 부분을 분리해라.
+    //단순하게 substring() 에 숫자를 직접 입력해서 문제를 풀면 된다.
+    public static void main(String[] args) {
+        String str = "hello.txt";
+        // 코드 작성
+        System.out.println("filename = " + str.substring(0, str.indexOf(".")));
+        System.out.println("extName = " + str.substring(str.indexOf(".")));
+    }
+    //실행 결과 ```
+    //filename = hello
+    //extName = .txt
+}
