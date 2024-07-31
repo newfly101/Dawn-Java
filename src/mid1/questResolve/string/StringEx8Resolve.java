56126377f54d874576726eaee021ffa46797396c
Jae-Hong-Kim <newfly101@naver.com>
2024-07-31 19:42:53 +0900

    3. String 클래스 관련 문제8 풀이 추가

---
+++ src/mid1/questResolve/string/StringEx8Resolve.java
@@ -0,0 +1,16 @@
+package mid1.questResolve.string;
+
+public class StringEx8Resolve {
+    // 문제8 - replace
+    //문제 설명
+    //replace() 를 사용해서 java 라는 단어를 jvm 으로 변경해라.
+     public static void main(String[] args) {
+     String input = "hello java spring jpa java";
+     // 코드 작성
+         System.out.println(input.replace("java", "jvm"));
+     }
+    //}
+    //```
+    //실행 결과 ```
+    //hello jvm spring jpa jvm
+}
