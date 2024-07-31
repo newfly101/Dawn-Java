2978d84cb395c534525aa3bccd5de447dfae7b02
Jae-Hong-Kim <newfly101@naver.com>
2024-07-31 19:25:09 +0900

    3. String 클래스 관련 문제1 풀이 추가

---
+++ src/mid1/questResolve/string/StringEx1Resolve.java
@@ -0,0 +1,14 @@
+package mid1.questResolve.string;
+
+public class StringEx1Resolve {
+    // 문제1 - startsWith
+    //문제 설명
+    //startsWith() 를 사용해서 url 이 https:// 로 시작하는지 확인해라.
+    public static void main(String[] args) {
+        String url = "https://www.example.com";
+         // 코드 작성
+        System.out.println(url.startsWith("https://"));
+    }
+    //실행 결과 ```
+    //true
+}
