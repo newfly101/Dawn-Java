63e7f8934e4893a114b6d8896c9fa070150879db
Jae-Hong-Kim <newfly101@naver.com>
2024-07-31 19:51:46 +0900

    3. String 클래스 관련 문제11 풀이 추가

---
+++ src/mid1/questResolve/string/StringEx11Resolve.java
@@ -0,0 +1,20 @@
+package mid1.questResolve.string;
+
+public class StringEx11Resolve {
+    // 문제11 - reverse()
+    //문제 설명
+    //str 문자열을 반대로 뒤집어라.
+    //StringBuilder 에 있는 reverse() 를 사용해라.
+    public static void main(String[] args) {
+        String str = "Hello Java";
+        // 코드 작성
+        StringBuilder sb = new StringBuilder();
+        String string = sb.append(str)
+                .reverse()
+                .toString();
+
+        System.out.println(string);
+    }
+    //실행 결과 ```
+    //avaJ olleH
+}
