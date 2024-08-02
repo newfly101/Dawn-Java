7824cc8d3dddbe09e87038f9172d923e8244b3e9
Jae-Hong-Kim <newfly101@naver.com>
2024-08-02 16:19:20 +0900

    4. Wrapper Class - 오토 박싱 / 오토 언박싱

---
+++ src/mid1/chap4Wrapper/AutoboxingMain2.java
@@ -0,0 +1,14 @@
+package mid1.chap4Wrapper;
+
+public class AutoboxingMain2 {
+    public static void main(String[] args) {
+        int value = 7;
+        Integer boxedValue = value; // 오토 박싱(Auto-boxing)
+
+        // Wrapper -> Primitive
+        int unboxedValue = boxedValue; // 오토 언박싱(Auto-Unboxing)
+
+        System.out.println("boxedValue = " + boxedValue);
+        System.out.println("unboxedValue = " + unboxedValue);
+    }
+}
